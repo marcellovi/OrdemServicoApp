aee03f03e6c3e3cb5fe90661bd5dbb88b3dd7558
Marcello F. Vieira <marcello.strategy@gmail.com>
2024-12-17 20:06:05 -0600

    Update edit.blade.php

--- resources/views/suprimentos/solicitacoes/os/edit.blade.php
+++ resources/views/suprimentos/solicitacoes/os/edit.blade.php
@@ -23,24 +23,24 @@
 
 @section('main')
 
-{{--    <div class="mb-4 col-md-12 mt-3" align="right">--}}
+    <div class="mb-4 col-md-12 mt-3" align="right">
 
-{{--        <button type="button" class="btn btn-info m-1" data-toggle="modal" data-target="#verifyModalContentConsultaEstoque"--}}
-{{--                data-whatever="@mdo">--}}
-{{--            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">--}}
-{{--                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>--}}
-{{--            </svg>--}}
-{{--            <span class="ul-btn__text">&nbsp;Consultar Estoque</span>--}}
-{{--        </button>--}}
-{{--        @if($solicitacao->status_id != 5)--}}
-{{--        <a href="{{ route('almoxarifado.solicitacao.compras.os.create',$solicitacao->id) }}" class="btn btn-info m-1">--}}
-{{--            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">--}}
-{{--                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z"/>--}}
-{{--            </svg>--}}
-{{--            <span class="ul-btn__text">&nbsp; Solicitar Compra</span>--}}
-{{--        </a>--}}
-{{--        @endif--}}
-{{--    </div>--}}
+        <button type="button" class="btn btn-info m-1" data-toggle="modal" data-target="#verifyModalContentConsultaEstoque"
+                data-whatever="@mdo">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+            </svg>
+            <span class="ul-btn__text">&nbsp;Consultar Estoque</span>
+        </button>
+        @if($solicitacao->status_id != 5)
+        <a href="{{ route('almoxarifado.solicitacao.compras.os.create',$solicitacao->id) }}" class="btn btn-info m-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+                <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z"/>
+            </svg>
+            <span class="ul-btn__text">&nbsp; Solicitar Compra</span>
+        </a>
+        @endif
+    </div>
 
     <!-- Editar Produto -->
     <div class="col-md mt-3">
