fa0c010329a1ad725903a48a9eefd42d52bd66f5
Marcello F. Vieira <marcello.strategy@gmail.com>
2024-12-17 18:39:32 -0600

    update migration

--- database/migrations/2024_12_06_223418_create_estoque_localizacao_table.php
+++ database/migrations/2024_12_06_223418_create_estoque_localizacao_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('estoque_localizacaos');
+        Schema::dropIfExists('estoque_localizacao');
     }
 };
--- database/migrations/2024_12_06_223440_create_fornecedores_table.php
+++ database/migrations/2024_12_06_223440_create_fornecedores_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('fornecedors');
+        Schema::dropIfExists('fornecedores');
     }
 };
